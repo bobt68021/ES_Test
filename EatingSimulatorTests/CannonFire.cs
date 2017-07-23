9a452ee3706e3e51d8b296fe3e51b2335696b28f
tishma <tishma.1@osu.edu>
2017-07-23 16:30:08 -0400

    it begins

---
+++ EatingSimulatorTests/CannonFire.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EatingSimulatorTests.Classes;
+
+namespace EatingSimulatorTests
+{
+    [TestClass]
+    public class CannonFire
+    {
+        [TestMethod]
+        public void TestFire()
+        {
+            Cannon cannon = new Cannon();
+            Assert.AreEqual(1, cannon.Shoot(1));
+        }
+
+        [TestMethod]
+        public void TestRotate()
+        {
+        }
+
+        [TestMethod]
+        public void TestAngleAdjust()
+        {
+        }
+    }
+}
---
+++ EatingSimulatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EatingSimulatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EatingSimulatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4096553c-0a9b-4c29-a247-c238d2d61284")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
